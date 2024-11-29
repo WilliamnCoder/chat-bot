bea093bf78588f64140784fe34609f624f7f3c57
William <121023616+Userwr20@users.noreply.github.com>
2024-11-29 19:06:15 +0100

    comments on code lines for clarification

--- ProjectBotCopy.py
+++ ProjectBotCopy.py
@@ -1,13 +1,13 @@
 
-name = input("Hello! What is your name? ")
+name = input("Hello! What is your name? ")  #Name variable and print statement with f-string based on answer
 print(f"Nice to meet you, {name}!")
 
-age_input = input("How old are you? ")
+age_input = input("How old are you? ") #Age variables defined and print statements 
 age = int(age_input)
 bot_age = 20
-age_difference = age - bot_age
+age_difference = age - bot_age   #The age difference number that will be printed in the next code line (9)
 print(f"You are {age_difference} years older than me. I am only {bot_age} years old!")
 
-color = input("What is your favorite color? ")
+color = input("What is your favorite color? ")  #Color variable defined and its print statements
 print("What is your favorite color? ")
-print(f"Oh, {color} is a beautiful color!")
+print(f"Oh, {color} is a beautiful color!") 
